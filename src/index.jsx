6de0e885d7b40fe6e5cb8a6f75c8e83b2ed69ff4
Mehmet Onur <onuur.s@gmail.com>
2019-04-10 14:53:30 +0200

    chore: update eslint config, install decimal.js and react-textarea-autosize feat: Invoice creator (wip)

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Invoice from './components/Invoice';
+import 'bootstrap/scss/bootstrap.scss';
+
+ReactDOM.render(<Invoice />, document.getElementById('root'));
